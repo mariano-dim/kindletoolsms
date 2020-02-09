1619cff2df6865051dcb1521524e6889c0eccfa3
Mariano Di Maggio <mariano.dim@gmail.com>
2020-02-09 02:22:23 -0300

    first commit

---
+++ src/main/java/com/ffx11/kindletoolsms/KindletoolsmsApplication.java
@@ -0,0 +1,13 @@
+package com.ffx11.kindletoolsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindletoolsmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KindletoolsmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ffx11/kindletoolsms/KindletoolsmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ffx11.kindletoolsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KindletoolsmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
